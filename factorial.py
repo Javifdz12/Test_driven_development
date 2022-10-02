09901b60081f1b5a0e3eaeaf8d01711b19f1ea20
Javifdz12 <101470100+Javifdz12@users.noreply.github.com>
2022-10-02 19:37:49 +0200

    gg

--- factorial.py
+++ factorial.py
@@ -1,6 +1,8 @@
 def factorial(num):
-    fact = 1
-    for i in range(1,num+1):
-        fact = fact * i
-    return fact
-
+    if num>=0:
+        fact = 1
+        for i in range(1,num+1):
+           fact = fact * i
+        return fact
+    else:
+        return 'No existe el factorial de un numero negativo'
