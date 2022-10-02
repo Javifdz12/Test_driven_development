03f3631e3e2e3ba8b9cb03aaa6456b88ec852755
Javifdz12 <101470100+Javifdz12@users.noreply.github.com>
2022-10-02 19:45:04 +0200

    gg

--- test_factorial.py
+++ test_factorial.py
@@ -2,3 +2,5 @@ import pytest
 from factorial import factorial
 def test_myfactorial():
     assert factorial(3) == 6
+def test_myfactorialneg():
+    assert factorial(-1)=='No existe el factorial de un numero negativo'
